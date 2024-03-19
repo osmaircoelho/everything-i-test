c518dcf2c49427d7a4bf3f5c5cbc1b3860bfaea9
osmaircoelho <osmaircoelho@gmail.com>
2024-03-19 17:20:10 +0000

    testing page-response-view

---
+++ tests/Feature/PageResponseViewTest.php
@@ -0,0 +1,11 @@
+<?php
+
+test('the products route is using the products view')
+   ->get('/products')
+   ->assertViewis('products');
+
+test('the products route is passing a list of products to the products view')
+    ->get('/products')
+    ->assertViewIs('products')
+    ->assertViewHas('products')
+;
