84e73f1f775726aeb95b01ba192e1edf48436704
osmaircoelho <osmaircoelho@gmail.com>
2024-03-28 13:03:46 -0300

    actions :: making sure that the action do what supposed to do

--- tests/Feature/ActionsTest.php
+++ tests/Feature/ActionsTest.php
@@ -3,6 +3,8 @@ use App\Actions\CreateProductAction;
 use App\Models\User;
 use Illuminate\Support\Facades\Notification;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
 
 it('should call the action to create a product', function (){
@@ -13,7 +15,6 @@ it('should call the action to create a product', function (){
     $this->mock(CreateProductAction::class)
         ->shouldReceive('handle')
         ->atLeast()->once();
-
     # Arrange
     $user = User::factory()->create();
     $title = 'Product 1';
@@ -23,4 +24,19 @@ it('should call the action to create a product', function (){
     # Act
     postJson(route('product.store'), ['title' => $title])
         ->assertCreated();
+});
+
+it('should be able to create a product', function () {
+
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    (new CreateProductAction())->handle('Product 1', $user);
+
+    assertDatabaseCount('products', 1);
+    assertDatabaseHas('products', [
+       'title' => 'Product 1',
+       'owner_id' => $user->id
+    ]);
 });
