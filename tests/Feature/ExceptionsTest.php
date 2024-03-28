ce7e1261a0de6d508f09bf833421aa6da46afade
osmaircoelho <osmaircoelho@gmail.com>
2024-03-28 18:43:13 -0300

    making sure that we are sending the correct data to a fake api

--- tests/Feature/ExceptionsTest.php
+++ tests/Feature/ExceptionsTest.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Console\Commands\CreateProductCommand;
+use App\Console\Commands\ExportProductToAmazon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use function Pest\Laravel\artisan;
 
 it('should be able to guarantee that the user exists', function (){
-    artisan(CreateProductCommand::class,
+    artisan(ExportProductToAmazon::class,
     ['title' => 'Osmair', 'user' => 99 ]
     );
 })->throws(ModelNotFoundException::class);
--- tests/Feature/HttpCallsTest.php
+++ tests/Feature/HttpCallsTest.php
@@ -26,3 +26,17 @@ it('should fake an api request', function () {
     assertDatabaseHas('products', ['title' => 'Product 2']);
     assertDatabaseCount('products', 2);
 });
+
+test('testing the data that we send to amazon', function () {
+    Http::fake();
+
+    Product::factory()->count(2)->create();
+
+    (new ExportProductToAmazon)->handle();
+
+    Http::assertSent(function(Request $request) {
+        return $request->url() == 'https://api.amazon.com/products'
+            && $request->header('Authorization') == ['Bearer 123']
+            && $request->data() == Product::all()->map(fn($p) => ['title' => $p->title])->toArray();
+    });
+});
